517bf9090c68892ea86352cda652d5d67841b150
asangalyevas69 <sabinaasangalyeva34@gmail.com>
2025-04-10 20:30:35 +0600

    Created admin/models

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Tag, Publication
+
+@admin.register(Category)
+class AdminCategory(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['id', 'name']
+    search_fields = ['name']
+
+@admin.register(Tag)
+class AdminTag(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['id', 'name']
+    search_fields = ['name']
+
+class TagsInline(admin.StackedInline):
+    model = Publication.tags.through
+    extra = 0
+
+
+@admin.register(Publication)
+class AdminPublication(admin.ModelAdmin):
+    list_display = ['id', 'name', 'author', 'created']
+    list_display_links = ['id', 'name','author', 'created']
+    list_filter = ['tags', 'category', 'author']
+    search_fields = ['name']
+    inlines = [TagsInline]
